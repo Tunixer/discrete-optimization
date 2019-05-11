576254eda3a4e239fbfcd552fe6cdd53412294b8
tunixer@gmail.com <tunixer@gmail.com>
2019-05-11 13:36:45 +0800

    Finish knapsack

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,30 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import queue
-q = queue.Queue()
-def DFS(colorArr, Graph, Node, colorNum):
-    if Node == len(colorArr):
-        return True;
-    for i in range(colorNum):
-        if IsColored(colorArr, Graph, Node, i):
-            colorArr[Node] = i
-            q.put(i)
-            temp = DFS(colorArr, Graph, Node+1, colorNum)
-            if temp == True:
-                return True
-            colorArr[Node] = -1
-            q.get()
-    return False
 
-'''
-whether Node can be colored with colorIdx
-'''
-def IsColored(colarArr, Graph, Node, colorIdx):
-    temp = Graph[Node]
-    for col in temp:
-        if colorIdx == colarArr[col]:
-            return False;
-    return True
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -35,30 +11,16 @@ def solve_it(input_data):
     first_line = lines[0].split()
     node_count = int(first_line[0])
     edge_count = int(first_line[1])
-    Graph = []
-    colorArr = []
-    for i in range(node_count):
-        Graph.append([])
-        colorArr.append(-1)
 
     edges = []
     for i in range(1, edge_count + 1):
         line = lines[i]
         parts = line.split()
-        Graph[int(parts[0])].append(int(parts[1]))
-        Graph[int(parts[1])].append(int(parts[0]))
+        edges.append((int(parts[0]), int(parts[1])))
 
     # build a trivial solution
     # every node has its own color
-    ask = False
-    if node_count < 1000:
-        ask = DFS(colorArr, Graph, 0, 5)
-    solution = []
-    if ask == True:
-        while not q.empty():
-            solution.append(q.get())
-
-
+    solution = range(0, node_count)
 
     # prepare the solution in the specified output format
     output_data = str(node_count) + ' ' + str(0) + '\n'
